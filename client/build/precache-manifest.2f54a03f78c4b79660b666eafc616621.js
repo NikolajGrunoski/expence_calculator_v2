a341374c19df96be75115e95045685ea60e95677
NikolajGrunoski <nikolaj.grunoski@gmail.com>
2020-02-10 13:06:38 +0100

    'test5'

---
+++ client/build/precache-manifest.2f54a03f78c4b79660b666eafc616621.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3795d80b1df3f461ce82a2e06fe7eaab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a49c5c0ce8d71ca15a66",
+    "url": "/static/css/main.259f59b4.chunk.css"
+  },
+  {
+    "revision": "241b2c9dc02b5ac5505c",
+    "url": "/static/js/2.87385be7.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/static/js/2.87385be7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a49c5c0ce8d71ca15a66",
+    "url": "/static/js/main.d1696c07.chunk.js"
+  },
+  {
+    "revision": "5536b1ffa5673e4eb964",
+    "url": "/static/js/runtime-main.d07f956b.js"
+  },
+  {
+    "revision": "cee72d8c528590bcdcfdf1ddf4b0954b",
+    "url": "/static/media/small_profile.cee72d8c.png"
+  }
+]);
